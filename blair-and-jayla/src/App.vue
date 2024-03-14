653b4b1181b6aff66cab68c508e7fa5062d81603
jaylaye <yejayla69@gmail.com>
2024-03-14 09:02:08 -0400

    .

--- blair-and-jayla/src/App.vue
+++ blair-and-jayla/src/App.vue
@@ -7,21 +7,20 @@
 </template>
 
 <script>
-import CardDisplay from './components/cardData.vue'
-import BarChart from 'path/to/component/BarChart'
+import cardData from './components/cardData.vue';
+
 
 export default {
   name: 'App',
 
-  components: 
-  { BarChart }
-  {
-    CardDisplay
+  components: {
+    cardData
   },
   data() {
     return {
-      data: []
-    }
+      data: [],
+      firstItem: {}
+    };
   },
   mounted() {
     this.fetchData()
@@ -29,28 +28,25 @@ export default {
   methods: {
     async fetchData() {
       try {
-        const response = await fetch('https://data.cityofnewyork.us/resource/k397-673e.json')
-        const data = await response.json()
+        const response = await fetch('https://data.cityofnewyork.us/resource/k397-673e.json');
+        const data = await response.json();
 
-        console.log('Data:', data)
+        console.log('Data:', data);
 
-        const firstItem = data[0]
+        const firstItem = data[0];
 
-        const fiscalYear = firstItem.fiscal_year
-        const payrollNumber = firstItem.payroll_number
-        const agencyName = firstItem.agency_name
+        const fiscalYear = firstItem.fiscal_year;
+        const agencyName = firstItem.agency_name;
 
-        console.log('Fiscal Year:', fiscalYear)
-        console.log('Payroll Number:', payrollNumber)
-        console.log('Agency Name:', agencyName)
+        console.log('Fiscal Year:', fiscalYear);
+        console.log('Payroll Number:', payrollNumber);
+        console.log('Agency Name:', agencyName);
 
-        this.firstItem = firstItem
+        this.firstItem = firstItem;
       } catch (error) {
-        console.error('Error fetching data:', error)
+        console.error('Error fetching data:', error);
       }
     }
   }
-}
+};
 </script>
-
-<style scoped></style>
