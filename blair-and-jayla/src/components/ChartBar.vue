abf4be5a7f2756fad11092a4ae7ffaabe7e5c440
blairweng7 <144455825+blairweng7@users.noreply.github.com>
2024-03-20 09:24:06 -0400

    safawe

---
+++ blair-and-jayla/src/components/ChartBar.vue
@@ -0,0 +1,8 @@
+<template>
+   <BarChart chart-data="" options="" css-classes = "chart-container"/>
+</template>
+
+<script setup>
+import { BarChart } from "vue-chart-3"
+</script>
+ 
--- blair-and-jayla/src/views/BarChart.vue
+++ blair-and-jayla/src/views/BarChart.vue
@@ -19,4 +19,25 @@
       }
     }
   });
+  const config = {
+  type: 'bar',
+  data: data,
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  },
+};
+data: {
+    datasets: [{
+        barPercentage: 0.5,
+        barThickness: 6,
+        maxBarThickness: 8,
+        minBarLength: 2,
+        data: [10, 20, 30, 40, 50, 60, 70]
+    }]
+};
+
 </script>
