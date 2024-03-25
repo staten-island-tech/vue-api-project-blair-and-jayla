eb40d1b27c5aa1d13d5f87e18a939a69771b9f2b
blairweng7 <144455825+blairweng7@users.noreply.github.com>
2024-03-25 09:02:08 -0400

    tujcv y

--- blair-and-jayla/src/views/BarChart.vue
+++ blair-and-jayla/src/views/BarChart.vue
@@ -1,48 +1,3 @@
-<<<<<<< HEAD
-<script>
-  const ctx = document.getElementById('myChart');
-
-  new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
-    }
-  });
-  const config = {
-  type: 'bar',
-  data: data,
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    }
-  },
-};
-data: {
-    datasets: [{
-        barPercentage: 0.5,
-        barThickness: 6,
-        maxBarThickness: 8,
-        minBarLength: 2,
-        data: [10, 20, 30, 40, 50, 60, 70]
-    }]
-};
-
-</script>
-=======
 <template>
   <Bar
     id="my-chart-id"
@@ -73,4 +28,3 @@ export default {
   }
 }
 </script>
->>>>>>> main
