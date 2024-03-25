d93a3aa757e7db67497b67a575a83086aef4bbd9
blairweng7 <144455825+blairweng7@users.noreply.github.com>
2024-03-25 09:22:15 -0400

    safeaw

--- blair-and-jayla/src/views/BarChart.vue
+++ blair-and-jayla/src/views/BarChart.vue
@@ -1,4 +1,7 @@
 <template>
+  <div class="container">
+    <Bar id= "my-chart-id" v-if="loaded" :options="chartOptions":data="chartData" />
+  </div>
   <Bar
     id="my-chart-id"
     :options="chartOptions"
@@ -15,16 +18,33 @@ ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 export default {
   name: 'BarChart',
   components: { Bar },
-  data() {
-    return {
-      chartData: {
-        labels: [ 'January', 'February', 'March' ],
-        datasets: [ { data: [40, 20, 12] } ]
-      },
-      chartOptions: {
-        responsive: true
-      }
+  data: () => ({
+    loaded: false,
+    chartData: null
+  }),
+  async mounted () {
+    this.loaded = false
+
+    try {
+      const { userlist } = await fetch('/api/userlist')
+      this.chartdata = userlist
+
+      this.loaded = true
+    } catch (e) {
+      console.error(e)
     }
   }
 }
+
+  // data() {
+  //   return {
+  //     chartData: {
+  //       labels: [ '2022', '2024' ],
+  //       datasets: [ { data: [40, 20, 12] } ]
+  //     },
+  //     chartOptions: {
+  //       responsive: true
+  //     }
+  //   }
+  // }
 </script>
