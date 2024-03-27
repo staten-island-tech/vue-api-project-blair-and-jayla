ea9a203e90f290c321632926970543f9601e3c5b
blairweng7 <144455825+blairweng7@users.noreply.github.com>
2024-03-26 22:52:42 -0400

    barchart

--- blair-and-jayla/src/components/ChartBar.vue
+++ blair-and-jayla/src/components/ChartBar.vue
@@ -51,36 +51,6 @@ export default  {
 
 </script>
 
-// export default{
-//    name: 'BarChart',
-//    components: {'Bar'},
-//    data() {
-//       loaded: false,
-//       chartData: null,
-//       chartOptions: {
-//          responsive: true,
-//          plugins: {
-//             display: true,
-//             labels: {
-//                color: white
-//             }
-//          }
-//       }
-//    },
-
-
-//  methods: {
-//   async onMounted(){
-//    try{
-//       let res = await fetch('https://data.cityofnewyork.us/resource/k397-673e.json')
-//       let data = await res.json()
-//       console.log(data)
-//    }
-// } 
-//  }
-
-</script>
-
 <style  scoped>
 
 </style>
